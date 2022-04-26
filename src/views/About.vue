15edc016edbc4869c86e58bd518608b79144965d
Maxime Moreillon <m.moreillon@gmail.com>
2022-04-26 17:30:15 +0900

    env

--- src/views/About.vue
+++ src/views/About.vue
@@ -6,7 +6,21 @@
       Camera viewer
     </v-card-title>
     <v-card-text>
-      Developed and maintained by <a href="https://maximemoreillon.com">Maxime Moreillon</a>
+      <p>Version {{version}}</p>
+      <p>Developed and maintained by <a href="https://maximemoreillon.com">Maxime Moreillon</a></p>
     </v-card-text>
   </v-card>
 </template>
+
+<script>
+import {version} from '../../package.json'
+
+export default {
+  nane: 'About',
+  data(){
+    return {
+      version,
+    }
+  }
+}
+</script>
