3141ab56b643c5bcb5acd2e08d3544c042b4d7ae
Maxime Moreillon <m.moreillon@gmail.com>
2022-07-23 00:13:57 +0000

    auth

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,9 +8,11 @@ for file in $ROOT_DIR/js/*.js* $ROOT_DIR/index.html $ROOT_DIR/precache-manifest*
 do
   echo "Processing $file ...";
 
-  sed -i 's|VUE_APP_AUTHENTICATION_API_URL_PLACEHOLDER|'${VUE_APP_AUTHENTICATION_API_URL}'|g' $file
   sed -i 's|VUE_APP_API_URL_PLACEHOLDER|'${VUE_APP_API_URL}'|g' $file
-  sed -i 's|VUE_APP_COOKIE_DOMAIN_PLACEHOLDER|'${VUE_APP_COOKIE_DOMAIN}'|g' $file
+  
+  sed -i 's|VUE_APP_IDENTIFICATION_URL_PLACEHOLDER|'${VUE_APP_IDENTIFICATION_URL}'|g' $file
+  sed -i 's|VUE_APP_LOGIN_URL_PLACEHOLDER|'${VUE_APP_LOGIN_URL}'|g' $file
+
 
 done
 
